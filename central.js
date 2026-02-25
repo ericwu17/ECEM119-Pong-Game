6b8159a6311a50196e772d943bf4336a5a15ffaa
Eric Wu <eric.dianhao.wu@gmail.com>
2026-02-24 22:09:35 -0800

    recognize face up/face down gesture

--- central.js
+++ central.js
@@ -3,9 +3,11 @@
 const noble = require('@abandonware/noble');
 
 const uuid_service = "a00693f9-f9ed-4fcc-8f89-e9b05dd65746"
-const uuid_value = "2102"
+const uuid_value1 = "2102"        // first characteristic
+const uuid_value2 = "2103"        // second characteristic (new)
 
-let sensorValue = NaN
+let sensorValue1 = NaN              // value for uuid_value1
+let sensorValue2 = NaN              // value for uuid_value2
 
 noble.on('stateChange', async (state) => {
     if (state === 'poweredOn') {
@@ -17,25 +19,38 @@ noble.on('stateChange', async (state) => {
 noble.on('discover', async (peripheral) => {
     await noble.stopScanningAsync();
     await peripheral.connectAsync();
-    const {
-        characteristics
-    } = await peripheral.discoverSomeServicesAndCharacteristicsAsync([uuid_service], [uuid_value]);
-    readData(characteristics[0])
+    const { characteristics } = await peripheral.discoverSomeServicesAndCharacteristicsAsync(
+        [uuid_service],
+        [uuid_value1, uuid_value2]
+    );
+    // characteristics array may contain both values; match by uuid
+    const char1 = characteristics.find(c => c.uuid === uuid_value1);
+    const char2 = characteristics.find(c => c.uuid === uuid_value2);
+    if (char1) readData1(char1);
+    if (char2) readData2(char2);
 });
 
 //
 // read data periodically
 //
-let readData = async (characteristic) => {
-    const value = (await characteristic.readAsync());
-    sensorValue = value.readFloatLE(0);
-    console.log(sensorValue);
-
-    // read data again in t milliseconds
+// helper that reads a characteristic repeatedly and stores into given variable
+let readData1 = async (characteristic) => {
+    const value = await characteristic.readAsync();
+    sensorValue1 = value.readFloatLE(0);
+    console.log("sensor1", sensorValue1);
     setTimeout(() => {
-        readData(characteristic)
+        readData1(characteristic);
     }, 10);
-}
+};
+
+let readData2 = async (characteristic) => {
+    const value = await characteristic.readAsync();
+    sensorValue2 = value.readFloatLE(0);
+    console.log("sensor2", sensorValue2);
+    setTimeout(() => {
+        readData2(characteristic);
+    }, 10);
+};
 
 //
 // hosting a web-based front-end and respond requests with sensor data
@@ -56,9 +71,10 @@ app.post('/', (req, res) => {
         'Content-Type': 'application/json'
     });
     res.end(JSON.stringify({
-        sensorValue: sensorValue
-    }))
-})
+        sensorValue: sensorValue1,
+        sensorValue2: sensorValue2
+    }));
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
